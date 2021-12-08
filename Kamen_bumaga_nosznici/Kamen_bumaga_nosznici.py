73d3be6fdc8989877c7ae1cfd2f551641a13bd08
opilane <opilane@HP-CZC6297BB7>
2021-12-08 10:51:30 +0200

    Poxhti gotovi 1vs1

--- Kamen_bumaga_nosznici/Kamen_bumaga_nosznici.py
+++ Kamen_bumaga_nosznici/Kamen_bumaga_nosznici.py
@@ -4,6 +4,7 @@ igra = ["ножницы","бумага","камень"]
 results1 =[]
 results2 =[]
 print("************************** Добро пожаловать в игру! **************************")
+print("********************************** Правила ***********************************")
 while 1: 
     try:
         vibor=int(input("Будете играть с человеком или ботом. 1 - с человекои; 2 - с ботом; => "))
@@ -21,28 +22,50 @@ if vibor == 1:
                 a=int(input("выберите номер: => "))
             except ValueError:
                 print()
-        if a==1:
-            a=igra[0]
-        elif a==2:
-            a=igra[1]
-        elif a==3:
-            a=igra[2]
         b=""
-        while a!=int and a not in [1,2,3]:
+        while b!=int and b not in [1,2,3]:
             try:
-                a=int(input("выберите номер: => "))
+                b=int(input("выберите номер: => "))
             except ValueError:
                 print()
-        if b==1:
-            b=igra[0]
-        elif b==2:
-            b=igra[1]
-        elif b==3:
-            b=igra[2]
-        if a==b:
+        if a == b:
             print("Ничья")
-        elif a == 1 and b == 2: 
+        elif a == 1 and b == 2: #Player1 - a and Player2 - b 
+            print("Победил Первый игрок!" )
+            results1.append(1)
+        elif a == 1 and b == 3:
+            print("Победил Второй игрок!")
+            results2.append(1)
+        elif a == 2 and b == 3:
             print("Победил Первый игрок!")
             results1.append(1)
+        elif a == 2 and b == 1:
+            print ("Победил Второй игрок!")
+            results2.append(1)
+        elif a == 3 and b == 1:
+            print("Победил Первый игрок!")
+            results1.append(1)
+        elif a == 3 and b == 2:
+            print("Победил Второй игрок!")
+            results2.append(1)
+        User1=sum(results1)
+        User2=sum(results2)
+        print(User1, User2)
+        print()
+        if User1 % 10 == 0 or User2 % 10 == 0 and User1!=0 or User2!=0:
+            while 1: 
+                try:
+                    trash=int(input("Обнуляем счет?  1 -да; 2 - нет; => "))
+                    if  trash==1 or trash==2:
+                        break
+                except ValueError:
+                    print()
+            if trash == 1:
+                results1.clear()
+                results2.clear()
+        print()
+
+
+            
 
 
